f1283291f3b7c25d00cd78b8397649ad3f591ec7
Dibya Jyoti Roy <djr-jsr@users.noreply.github.com>
2024-10-29 02:56:01 -0700

    Add auto versioning

--- src/Components/FooterComponent.tsx
+++ src/Components/FooterComponent.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react';
-import { Box, IconButton } from '@mui/material';
+import { Box, Grid, IconButton, Theme, Typography } from '@mui/material';
 import { GitHub } from '@mui/icons-material';
+import { LIB_VERSION } from '../version';
+
+const displayFontStyle = (_theme: Theme) => ({
+    fontWeight: 'bold',
+    input: { textAlign: 'center' }
+});
 
 const FooterComponent: FC = () => {
 
@@ -17,9 +23,14 @@ const FooterComponent: FC = () => {
                 borderRadius: 1
             }}
         >
-            <IconButton size='large' color='inherit' onClick={() => window.open('https://github.com/djr-jsr/datetime', '_blank')}>
-                <GitHub fontSize='large' />
-            </IconButton>
+            <Grid container direction='column' display='flex' alignItems='center' justifyItems='center' spacing={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}>
+                <IconButton size='large' color='inherit' onClick={() => window.open('https://github.com/djr-jsr/datetime', '_blank')}>
+                    <GitHub fontSize='large' />
+                </IconButton>
+                <Typography noWrap sx={displayFontStyle}>
+                    {LIB_VERSION || '0.0.0'}
+                </Typography>
+            </Grid>
         </Box>
     );
 };
